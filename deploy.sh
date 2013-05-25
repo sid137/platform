10098ca780c4030d418603049591462bef8c8fa6
Sidney Burks <sid137@gmail.com>
2013-05-25 17:15:23 +0200

    platform deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-bundle exec knife digital_ocean droplet create --image 25489 --size 66 \
-  --location 1 --ssh-keys 7798 --bootstrap \
-  --solo \
-  --server-name comp2 \
+# To launch and configure a server, run:
+#     ./deploy server-name
+
+server_name=$1
+
+bundle exec knife digital_ocean droplet create \
+  --image 25489              \
+  --size 66                  \
+  --location 1               \
+  --ssh-keys 7798            \
+  --bootstrap                \
+  --solo                     \
+  --server-name $server_name \
   --run-list "recipe[platform]"
